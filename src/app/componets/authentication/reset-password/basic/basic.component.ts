38721bb5a729028f9c44a364706102944be7a856
vijay <vijaybits9892@gmail.com>
2024-10-27 17:56:54 +0530

    forgot password  otp changes

--- src/app/componets/authentication/reset-password/basic/basic.component.ts
+++ src/app/componets/authentication/reset-password/basic/basic.component.ts
@@ -28,8 +28,12 @@ export class BasicComponent extends BaseComponent implements OnInit {
   //   "password": "string"
   // }
   confirmPassword:any = ''; newPassword:any = ''; email:any = ''; submt = true; submitted = false; 
-  isEmailDisable = false; isOtpDisable = false; isBtndisable = true;
-  otp: any = '';
+  isEmailDisable = false; isOtpDisable = false; isBtndisable = true; isEmail = true;
+  // otp: any = '';
+  otpArray = Array(6).fill(null);
+  otp: string[] = Array(this.otpArray.length).fill('');
+  isOtpValid = true;
+  errorMessage = ''; // Holds error messages
   resetFrm: FormGroup = this.fb.group({ 
     // email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, this.passwordValidator]],
@@ -101,9 +105,9 @@ export class BasicComponent extends BaseComponent implements OnInit {
     if (this.email == ''){
       this.toastr.warning('please enter email ','reset', { timeOut: 3000, positionClass: 'toast-top-right'})
     } 
-    else if(this.otp == '') {
-      this.toastr.warning('please enter OTP','reset', { timeOut: 3000, positionClass: 'toast-top-right'})
-    }
+    // else if(this.otp == '') {
+    //   this.toastr.warning('please enter OTP','reset', { timeOut: 3000, positionClass: 'toast-top-right'})
+    // }
     // else if(this.verify == '') {
     //   this.toastr.warning('please enter OTP','reset', { timeOut: 3000, positionClass: 'toast-top-right'})
     // }
@@ -112,11 +116,12 @@ export class BasicComponent extends BaseComponent implements OnInit {
       next: (res:any) => {
         if(res.status == true){
           // this.router.navigate(['/authentication/sign-in/basic']);
-          this.submt = false;
+          
           this.toastr.success(res.message,'reset password', {
             timeOut: 3000,
             positionClass: 'toast-top-right',
-          });
+          }),
+          this.onMailCheck();
         } else {
           this.toastr.error(res.message,'reset password', {
             timeOut: 3000,
@@ -135,7 +140,7 @@ export class BasicComponent extends BaseComponent implements OnInit {
     } else {
       this.switchService.onMailValidReset(this.email).subscribe({ next: (res:any) => {
         if(res.status == true){
-          this.isEmailDisable = true;
+          this.isEmail = false; 
           this.toastr.success(res.message,'', {
             timeOut: 3000, positionClass: 'toast-top-right' });
           } else {
@@ -147,20 +152,47 @@ export class BasicComponent extends BaseComponent implements OnInit {
     }
   }
 
+  
+
+  onInputChange(event: Event, index: number): void {
+    const input = event.target as HTMLInputElement;
+    this.otp[index] = input.value;
+
+    if (input.value && index < this.otpArray.length - 1) {
+      (document.querySelectorAll('.otp-container input')[index + 1] as HTMLInputElement)?.focus();
+    }
+  }
+
+  onKeydown(event: KeyboardEvent, index: number): void {
+    if (event.key === 'Backspace' && index > 0 && !this.otp[index]) {
+      (document.querySelectorAll('.otp-container input')[index - 1] as HTMLInputElement)?.focus();
+    }
+  }
+
+
   onOtpCheck(){
-    if(this.email == ''){
-      this.toastr.warning('Please enter email','', {
-      timeOut: 3000, positionClass: 'toast-top-right'});
-    } else if(this.otp == ''){
-      this.toastr.warning('Please enter OTP','', {
-      timeOut: 3000, positionClass: 'toast-top-right'});
-    } else {
-      this.switchService.onOtpSignup(this.email, this.otp).subscribe({ next: (res:any) => {
+    const enteredOtp = this.otp.join('');
+
+    // Check if OTP length is less than 6
+    if (enteredOtp.length < 6) {
+      this.isOtpValid = false;
+      this.errorMessage = 'Please enter the complete OTP.';
+      this.toastr.warning('Please enter the complete OTP','', {
+        timeOut: 3000, positionClass: 'toast-top-right'});
+      return;
+    }
+    // if(this.otp == ''){
+    //   this.toastr.warning('Please enter OTP','', {
+    //   timeOut: 3000, positionClass: 'toast-top-right'});
+    // } 
+    else {
+      this.switchService.onOtpSignup(this.email, enteredOtp).subscribe({ next: (res:any) => {
         if(res.status == true){
-          this.isOtpDisable = true, this.isBtndisable = false,
+          this.isOtpDisable = true, this.isBtndisable = false, this.submt = false;
           this.toastr.success(res.message,'reset', {
             timeOut: 3000, positionClass: 'toast-top-right' });
           } else {
+            // this.errorMessage = 'Invalid OTP. Please try again.';
             this.toastr.error(res.message,'reset', {
               timeOut: 3000, positionClass: 'toast-top-right' });
           }
