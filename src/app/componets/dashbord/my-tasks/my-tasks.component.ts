f386d9bd72c38823cd7355f175432f04d674995d
vengamma <vengamma523@gmail.com>
2024-11-29 15:23:16 +0530

    my-tasks changes

--- src/app/componets/dashbord/my-tasks/my-tasks.component.ts
+++ src/app/componets/dashbord/my-tasks/my-tasks.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import flatpickr from 'flatpickr';
 import { FlatpickrDefaults, FlatpickrModule } from 'angularx-flatpickr';
 import { SharedModule } from '../../../../app/shared/common/sharedmodule';
 import { NgbDropdownModule,NgbNavModule,NgbModal, NgbModalConfig, NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { FilePondComponent, FilePondModule } from 'ngx-filepond';
+import * as FilePond from 'filepond';
 @Component({
   selector: 'app-my-tasks',
   standalone: true,
   imports: [SharedModule, NgSelectModule, NgbModule,
-    NgbNavModule,NgbDropdownModule,FlatpickrModule,FormsModule,ReactiveFormsModule],
+    NgbNavModule,NgbDropdownModule,FlatpickrModule,FormsModule,ReactiveFormsModule,FilePondModule],
   providers: [NgbModalConfig, NgbModal,FlatpickrDefaults],
   templateUrl: './my-tasks.component.html',
   styleUrl: './my-tasks.component.scss'
 })
 export class MyTasksComponent {
+  @ViewChild("myPond") myPond!: FilePondComponent;
+  pondOptions: FilePond.FilePondOptions = {
+    allowMultiple: true,
+    labelIdle: "Drop files here to Upload...",
+  };
+  singlepondOptions: FilePond.FilePondOptions = {
+    allowMultiple: false,
+    labelIdle: "Drop files here to Upload...",
+  };
+  pondFiles: FilePond.FilePondOptions["files"] = [
+    {
+      source: "assets/photo.jpeg",
+      options: {
+        type: "local",
+      },
+    },
+  ];
 
   modal: any;
     constructor(// config: NgbModalConfig,
@@ -36,5 +54,16 @@ export class MyTasksComponent {
         dateFormat: 'H:i',
       };
       flatpickr('#addignedDate', this.flatpickrOptions);
+  }
+  
+  pondHandleInit() {
+    console.log("FilePond has initialised", this.myPond);
+  }
+
+  pondHandleAddFile(event: any) {
+    console.log("A file was added", event);
+  }
+  pondHandleActivateFile(event: any) {
+    console.log("A file was activated", event);
   }
 }
